038370ce7a1afc8d526be9a489f3374cc7371d5f
Md Sourov Hasan <mdsourovhasan2552@gmail.com>
2026-02-05 20:38:20 +0600

    go_ router start

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:go_router/screens/home_page.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized(); // ensure everything initialized
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Rent App',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home:HomePage(), 
+    );
+  }
+}
